410eeba356fccfe5ef1b91dfb50e1479dea2d9c4
butaier <tochenyuxiao@163.com>
2018-01-04 09:51:38 +0800

    git add ignore

--- fs/stream.js
+++ fs/stream.js
@@ -44,4 +44,6 @@ var rs = fs.createReadStream('testFile.txt');
 var ws = fs.createWriteStream('forPipe.txt');
 rs.pipe(ws);
 // rs.pipe(ws, { end: false });//默认情况下，当Readable流的数据读取完毕，end事件触发后，将自动关闭Writable流。如果我们不希望自动关闭Writable流，需要传入参数：
-
+  
+  // aaa  
+  //	a
